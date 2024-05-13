f591bb2d457e6ddef2d30870d1946ab634f9376b
morris <dltkdah777@gmail.com>
2024-05-13 13:08:40 +0900

    project start

---
+++ src/main/java/com/morris/bikeroutespring/BikeRouteSpringApplication.java
@@ -0,0 +1,13 @@
+package com.morris.bikeroutespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRouteSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeRouteSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morris/bikeroutespring/BikeRouteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morris.bikeroutespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRouteSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
